05cb77660c2c16fed24718a5187751025fd9e319
lucasfaggion <106668079+lucascriadefavela@users.noreply.github.com>
2024-08-19 09:50:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,58 +6,82 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Como a superpopulação pode impactar a incidência de crimes como o estupro?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Aumento da tensão social",
+                afirmacao: "Em áreas superpovoadas, a competição por recursos pode aumentar a tensão entre as pessoas, potencialmente levando a um aumento na criminalidade, incluindo estupros."
+            },
+            {
+                texto: "Falta de recursos e apoio",
+                afirmacao: " A superpopulação pode resultar em serviços públicos insuficientes, como segurança e apoio às vítimas, o que pode dificultar a denúncia e o combate ao estupro."
+            } 
+            {
+                texto: "Desigualdade social",
+                afirmacao: "Regiões superpopuladas frequentemente enfrentam desigualdades sociais, que podem contribuir para a desvalorização da vida humana e, consequentemente, para a violência sexual."
+            }  
+            {
+                texto: "Normalização da violência",
+                afirmacao: "Em ambientes onde a violência é comum, a cultura pode se tornar mais permissiva em relação a atos como o estupro, levando a um aumento na sua incidência."
+            }
+             {
+                texto: "Dificuldades na educação",
+                afirmacao: "Em sociedades superpopuladas, a educação sobre consentimento e direitos humanos pode ser negligenciada, resultando em uma maior vulnerabilidade a abusos.."
+            }
+            
+        ]
+    },
+    {
+        enunciado: "De que maneira as doutrinas religiosas influenciam a percepção e o tratamento do estupro nas sociedades?",
+        alternativas: [
+            {
+                texto: "Estigmatização das vítimas",
                 afirmacao: "afirmação"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmação"
+                texto: "Escrevafire o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "Algumas doutrinas religiosas podem perpetuar a ideia de que as vítimas têm responsabilidade sobre o ato, levando a um estigma que dificulta a denúncia e o apoio."
+            }
+             {
+                texto: "Valores de perdão e reconciliação",
+                afirmacao: "2. Muitas religiões enfatizam o perdão, o que pode levar as vítimas a se sentirem pressionadas a perdoar o agressor, em vez de buscar justiça."
+            }
+             {
+                texto: "Educação moral",
+                afirmacao: "Algumas religiões promovem a educação moral que pode ajudar a prevenir a violência sexual, enfatizando o respeito e o consentimento.A forma como as instituições religiosas tratam casos de estupro pode variar, com algumas adotando uma abordagem mais protetora das vítimas e outras priorizando a proteção da imagem da instituição."
+            }
+             {
+                texto: "Influência nas leis",
+                afirmacao: "5. Em algumas sociedades, as doutrinas religiosas influenciam a legislação, o que pode afetar a forma como os casos de estupro são tratados legalmente."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Qual o papel das religiões na discussão sobre controle populacional em contextos de superpopulação?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "afirmação"
+                texto: "Resistência a métodos contraceptivos",
+                afirmacao: " Algumas religiões se opõem ao uso de contraceptivos, o que pode dificultar a implementação de políticas de controle populacional em comunidades superpovoadas."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "afirmação"
+                texto: "Promoção da família",
+                afirmacao: "Muitas tradições religiosas valorizam a família grande, o que pode contribuir para a superpopulação e a resistência a iniciativas de planejamento familiar."
+            }
+              {
+                texto: "Educação e conscientização",
+                afirmacao: "Algumas religiões têm se engajado em diálogos sobre a importância do planejamento familiar e do controle populacional como parte de uma abordagem ética."
+            } 
+    {
+                texto: "Interpretação de textos sagrados"
+                afirmacao: " A forma como textos sagrados são interpretados pode influenciar a aceitação ou rejeição de políticas de controle populacional nas comunidades religiosas."
+            }
+  {
+                texto: "Apoio a políticas sociais",
+                afirmacao: "5. Algumas organizações religiosas têm apoiado iniciativas que promovem a saúde reprodutiva e o empoderamento das mulheres, contribuindo para um controle populacional mais eficaz. "
             }
         ]
     },
-    {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "afirmação"
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "afirmação"
-            }
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "afirmação"
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "afirmação"
-            }
-        ]
-    },
-    {
+   
         enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
         alternativas: [
             {
